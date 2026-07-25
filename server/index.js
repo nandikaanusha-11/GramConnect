d9afaa5efd489491dbe033c4b128e4369088333b
nandikaanusha-11 <nandikaanusha11@gmail.com>
2026-07-25 21:47:24 +0530

    Backend part for Authentication and authorisation was done

--- server/index.js
+++ server/index.js
@@ -5,7 +5,8 @@ require("dotenv").config();
 const db = require("./config/db");
 
 const app = express();
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 app.use(cors());
 app.use(express.json());
 
@@ -109,6 +110,195 @@ app.post("/grievances", (req, res) => {
     );
 
 });
+
+
+
+app.post("/admin/signup", async (req, res) => {
+
+    const { name, email, password } = req.body;
+
+    const checkAdmin =
+        "SELECT * FROM admins WHERE email = ?";
+
+    db.query(checkAdmin, [email], async (err, result) => {
+
+        if (err) {
+
+            console.log(err);
+
+            return res.status(500).json({
+                message: "Database Error"
+            });
+
+        }
+
+        if (result.length > 0) {
+
+            return res.status(400).json({
+                message: "Admin already exists"
+            });
+
+        }
+
+        const hashedPassword =
+            await bcrypt.hash(password, 10);
+
+        const sql =
+            "INSERT INTO admins(name,email,password) VALUES(?,?,?)";
+
+        db.query(
+            sql,
+            [name, email, hashedPassword],
+            (err) => {
+
+                if (err) {
+
+                    console.log(err);
+
+                    return res.status(500).json({
+                        message: "Signup Failed"
+                    });
+
+                }
+
+                res.status(201).json({
+                    message: "Admin Registered Successfully"
+                });
+
+            }
+        );
+
+    });
+
+});
+
+
+
+app.post("/admin/login", (req, res) => {
+
+    const { email, password } = req.body;
+
+    const sql =
+        "SELECT * FROM admins WHERE email = ?";
+
+    db.query(sql, [email], async (err, result) => {
+
+        if (err) {
+
+            console.log(err);
+
+            return res.status(500).json({
+                message: "Database Error"
+            });
+
+        }
+
+        if (result.length === 0) {
+
+            return res.status(404).json({
+                message: "Admin Not Found"
+            });
+
+        }
+
+        const admin = result[0];
+
+        const isMatch =
+            await bcrypt.compare(password, admin.password);
+
+        if (!isMatch) {
+
+            return res.status(401).json({
+                message: "Invalid Password"
+            });
+
+        }
+
+        const token = jwt.sign(  //jwt.sign()-create a new jwt
+
+            {//paylload-info to store inside the token(non-sensitive info)
+                admin_id: admin.admin_id,
+                email: admin.email
+            },
+
+            process.env.JWT_SECRET,
+
+            {
+                expiresIn: "1d" //token valid only for one day
+            }
+
+        );
+
+        res.status(200).json({
+
+            message: "Login Successful",
+
+            token,
+
+            admin: {
+
+                admin_id: admin.admin_id,
+
+                name: admin.name,
+
+                email: admin.email
+
+            }
+
+        });
+
+    });
+
+});
+
+  
+app.put("/grievances/:id", (req, res) => {
+
+    const grievanceId = req.params.id;
+
+    const { status } = req.body;
+
+    const sql = `
+        UPDATE grievances
+        SET status = ?
+        WHERE grievance_id = ?
+    `;
+
+    db.query(
+        sql,
+        [status, grievanceId],
+        (err, result) => {
+
+            if (err) {
+
+                console.log(err);
+
+                return res.status(500).json({
+                    message: "Failed to update grievance status"
+                });
+
+            }
+
+            if (result.affectedRows === 0) {
+
+                return res.status(404).json({
+                    message: "Grievance not found"
+                });
+
+            }
+
+            res.status(200).json({
+                message: "Status updated successfully"
+            });
+
+        }
+    );
+
+});
+
+    
+
+
 
 
 const PORT = process.env.PORT || 5000;
